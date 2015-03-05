5478e06c9ce5ef0efd1955238b498442a3b4864a
Jacob <TealNerd@users.noreply.github.com>
2015-03-05 12:08:29 -0600

    Update FriendRequestCommand.java
    Can now send requests to whole groups of people

--- src/main/java/com/biggestnerd/civfriends/commands/FriendRequestCommand.java
+++ src/main/java/com/biggestnerd/civfriends/commands/FriendRequestCommand.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 
 import vg.civcraft.mc.namelayer.NameAPI;
 import vg.civcraft.mc.namelayer.command.PlayerCommand;
+import vg.civcraft.mc.namelayer.group.Group;
 
 import com.biggestnerd.civfriends.CivFriends;
 import com.biggestnerd.civfriends.FriendList;
@@ -26,7 +27,7 @@ public class FriendRequestCommand extends PlayerCommand {
 		setIdentifier("fr");
 		setDescription("Sends a friend request to a player");
 		setUsage("/fr <playername>");
-		setArguments(1,1);
+		setArguments(1,2);
 	}
 
 	@Override
@@ -37,6 +38,28 @@ public class FriendRequestCommand extends PlayerCommand {
 		}
 		Player p = (Player) sender;
 		UUID executor = NameAPI.getUUID(p.getName());
+		if(args.length == 2 && args[0].equalsIgnoreCase("group")) {
+			String groupName = args[1];
+			Group group = NameAPI.getGroupManager().getGroup(groupName);
+			if(group == null) {
+				p.sendMessage(ChatColor.RED + "That group doesn't exist");
+				return true;
+			}
+			if(!group.getAllMembers().contains(executor)) {
+				p.sendMessage(ChatColor.RED + "You are not part of that group");
+				return true;
+			}
+			for(UUID uuid : group.getAllMembers()) {
+				if(uuid.equals(executor)) {
+					//do nothing
+				} else {
+					if(!FriendshipListener.hasInivte(uuid, executor))
+						FriendshipListener.addNotification(uuid, executor);
+				}
+				p.sendMessage(ChatColor.GREEN + "Successfully sent friend requests to all members of the group " + groupName);
+				return true;
+			}
+		}
 		UUID uuid = NameAPI.getUUID(args[0]);
 		if(uuid == null) {
 			p.sendMessage(ChatColor.RED + "That player has never played before.");
